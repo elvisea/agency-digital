d832d0652a93c963ad5129eb1aec4efbcd9ab206
elvis <elvis.e.amancio@gmail.com>
2021-05-05 22:15:33 -0300

    Create Landing Page 1-5

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import styles from "../styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <header>
+        <Image src="/images/logo.svg" alt="Logo Empresa" height={61} width={259} />
+        <nav>
+          <a href="http://">Home</a>
+          <a href="http://">Seus benefícios</a>
+          <a href="http://">Nosso processo</a>
+          <a href="http://">Nossos trabalhos</a>
+          <a href="http://">Testemunhos</a>
+          <a href="http://">Inicie um projeto</a>
+        </nav>
+        <button>GET STARTED</button>
+      </header>
+
+      <div className={styles.containerContent}>
+        <h1>Uma agência de design de<br /> produto digital</h1>
+        <h2>Somos Agência Digital Criativa e Profissional</h2>
+        <button>GET STARTED</button>
+        <Image  src="/images/hero.svg" alt="" height={500} width={700} />
+      </div>
+    </div>
+  )
+}
